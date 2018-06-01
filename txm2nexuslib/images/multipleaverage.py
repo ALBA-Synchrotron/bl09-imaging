062ce02e003628959e8a61791910fc6db0bae798
mrosanes <mrosanes@cells.es>
2018-06-01 16:17:44 +0200

    Adapt multipleaverage for magnetism experiments
    Adapt multipleaverage to be used for magnetism experiments
    where we need to average images taken for same jj positions
    and same angle, but different repetitions.
    
    Many images are taken at same angle and jj positions and
    averaged in order to increase the SNR.

--- txm2nexuslib/images/multipleaverage.py
+++ txm2nexuslib/images/multipleaverage.py
@@ -68,48 +68,81 @@ def average_and_store(group_to_average_image_filenames,
                   "angle": angle, "average": True, "avg_by": "zpz",
                   "zpz": zp_central, "zpz_central": zp_central}
 
-        img_in_obj = Image(images_to_average_filenames[0], mode="r")
-        h5_in = img_in_obj.f_h5_handler
-        img_avg_obj = Image(output_complete_fn)
-        h5_avg = img_avg_obj.f_h5_handler
+    elif variable == "repetition":
+        num_repetitions = group_to_average_image_filenames[0]
+        images_to_average_filenames = group_to_average_image_filenames[1]
+        date_sample_energy_jj_angle = group_to_average_image_filenames[2]
+        fn_first = images_to_average_filenames[0]
+        dir_name = os.path.dirname(fn_first)
 
-        metadata_in = "metadata"
-        metadata_out = "metadata"
-        if metadata_in in h5_in:
-            meta_in_grp = h5_in[metadata_in]
-            if metadata_out not in h5_avg:
-                meta_out_grp = h5_avg.create_group(metadata_out)
-            if "/metadata/energy" in h5_in:
-                energy = round(meta_in_grp["energy"].value, 1)
-                meta_out_grp.create_dataset("energy", data=energy)
-                meta_out_grp["energy"].attrs["units"] = "eV"
-            if "/metadata/angle" in h5_in:
-                angle = round(meta_in_grp["angle"].value, 2)
-                if angle == 0:
-                    angle = 0.0
-                meta_out_grp.create_dataset("angle", data=angle)
-                meta_out_grp["angle"].attrs["units"] = "degree"
-            if "/metadata/pixel_size" in h5_in:
-                pixel_size = meta_in_grp["pixel_size"].value
-                meta_out_grp.create_dataset("pixel_size", data=pixel_size)
-                meta_out_grp["pixel_size"].attrs["units"] = "um"
-            if "/metadata/magnification" in h5_in:
-                magnification = meta_in_grp["magnification"].value
-                meta_out_grp.create_dataset("magnification",
-                                            data=magnification)
-            if "/metadata/exposure_time" in h5_in:
-                exposure_time = meta_in_grp["exposure_time"].value
-                meta_out_grp.create_dataset("exposure_time",
-                                            data=exposure_time)
-                meta_out_grp["exposure_time"].attrs["units"] = "s"
-            if "/metadata/machine_current" in h5_in:
-                machine_current = meta_in_grp["machine_current"].value
-                meta_out_grp.create_dataset("machine_current",
-                                            data=machine_current)
-                meta_out_grp["machine_current"].attrs["units"] = "mA"
+        date = date_sample_energy_jj_angle[0]
+        sample = date_sample_energy_jj_angle[1]
+        energy = date_sample_energy_jj_angle[2]
+        jj_u = date_sample_energy_jj_angle[3]
+        jj_d = date_sample_energy_jj_angle[4]
+        angle = date_sample_energy_jj_angle[5]
 
-        h5_in.close()
-        img_avg_obj.close_h5()
+        jj_offset = round((jj_u + jj_d)/2.0, 1)
+
+        output_fn = (str(date) + "_" + str(sample) + "_" + str(energy) +
+                     "_" + str(jj_offset) + "_" + str(angle) +
+                     "_avg_repetitions.hdf5")
+        output_complete_fn = dir_name + "/" + output_fn
+        average_images(images_to_average_filenames,
+                       dataset_for_average=dataset_for_averaging,
+                       description=description, store=True,
+                       output_h5_fn=output_complete_fn,
+                       dataset_store=dataset_store)
+
+        # Store metadata: extracting metadata from repetition 0
+        record = {"filename": output_fn, "extension": ".hdf5",
+                  "date": date, "sample": sample, "energy": energy,
+                  "jj_u": jj_u, "jj_d": jj_d, "jj_offset": jj_offset,
+                  "angle": angle, "average": True, "avg_by": "repetition",
+                  "num_repetitions": num_repetitions}
+
+    img_in_obj = Image(images_to_average_filenames[0], mode="r")
+    h5_in = img_in_obj.f_h5_handler
+    img_avg_obj = Image(output_complete_fn)
+    h5_avg = img_avg_obj.f_h5_handler
+
+    metadata_in = "metadata"
+    metadata_out = "metadata"
+    if metadata_in in h5_in:
+        meta_in_grp = h5_in[metadata_in]
+        if metadata_out not in h5_avg:
+            meta_out_grp = h5_avg.create_group(metadata_out)
+        if "/metadata/energy" in h5_in:
+            energy = round(meta_in_grp["energy"].value, 1)
+            meta_out_grp.create_dataset("energy", data=energy)
+            meta_out_grp["energy"].attrs["units"] = "eV"
+        if "/metadata/angle" in h5_in:
+            angle = round(meta_in_grp["angle"].value, 2)
+            if angle == 0:
+                angle = 0.0
+            meta_out_grp.create_dataset("angle", data=angle)
+            meta_out_grp["angle"].attrs["units"] = "degree"
+        if "/metadata/pixel_size" in h5_in:
+            pixel_size = meta_in_grp["pixel_size"].value
+            meta_out_grp.create_dataset("pixel_size", data=pixel_size)
+            meta_out_grp["pixel_size"].attrs["units"] = "um"
+        if "/metadata/magnification" in h5_in:
+            magnification = meta_in_grp["magnification"].value
+            meta_out_grp.create_dataset("magnification",
+                                        data=magnification)
+        if "/metadata/exposure_time" in h5_in:
+            exposure_time = meta_in_grp["exposure_time"].value
+            meta_out_grp.create_dataset("exposure_time",
+                                        data=exposure_time)
+            meta_out_grp["exposure_time"].attrs["units"] = "s"
+        if "/metadata/machine_current" in h5_in:
+            machine_current = meta_in_grp["machine_current"].value
+            meta_out_grp.create_dataset("machine_current",
+                                        data=machine_current)
+            meta_out_grp["machine_current"].attrs["units"] = "mA"
+
+    h5_in.close()
+    img_avg_obj.close_h5()
 
     return record
 
@@ -128,14 +161,14 @@ def average_image_groups(file_index_fn, table_name="hdf5_proc",
 
     """
     TODO: In the future it should be made available, the
-     average by variable == repetition.
-     For the moment only the average by variable == zpz is implemented
+     average by variable == repetition and just after by
+     variable == zpz.
 
      Finally this three features should exist:
-     - average by same angle and different zpz positions
-     - average by same angle, same zpz and different repetition
+     - average by same angle and different zpz positions (DONE)
+     - average by same angle, same zpz and different repetition (ONGOING)
      - average by same angle, first by same zpz and different repetition,
-     and afterwards by same angle and different zpz positions
+     and afterwards by same angle and different zpz positions (TODO)
     """
 
     start_time = time.time()
@@ -155,19 +188,19 @@ def average_image_groups(file_index_fn, table_name="hdf5_proc",
     all_file_records = file_index_db.all()
     n_files = len(all_file_records)
 
-    dates_samples_energies_angles = []
-    for record in all_file_records:
-        dates_samples_energies_angles.append((record["date"],
-                                              record["sample"],
-                                              record["energy"],
-                                              record["angle"]))
-    dates_samples_energies_angles = list(set(dates_samples_energies_angles))
-
     averages_table = db.table("hdf5_averages")
     averages_table.purge()
 
     groups_to_average = []
     if variable == "zpz":
+        dates_samples_energies_angles = []
+        for record in all_file_records:
+            dates_samples_energies_angles.append((record["date"],
+                                                  record["sample"],
+                                                  record["energy"],
+                                                  record["angle"]))
+        dates_samples_energies_angles = list(
+            set(dates_samples_energies_angles))
         for date_sample_energy_angle in dates_samples_energies_angles:
             date = date_sample_energy_angle[0]
             sample = date_sample_energy_angle[1]
@@ -194,6 +227,48 @@ def average_image_groups(file_index_fn, table_name="hdf5_proc",
             central_zpz_with_group_to_average.append(group_to_average)
             central_zpz_with_group_to_average.append(date_sample_energy_angle)
             groups_to_average.append(central_zpz_with_group_to_average)
+            print("------")
+            print(central_zpz_with_group_to_average)
+            print("------")
+
+    elif variable == "repetition":
+        dates_samples_energies_jjs_angles = []
+        for record in all_file_records:
+            dates_samples_energies_jjs_angles.append((record["date"],
+                                                      record["sample"],
+                                                      record["energy"],
+                                                      record["jj_u"],
+                                                      record["jj_d"],
+                                                      record["angle"]))
+        dates_samples_energies_jjs_angles = list(
+            set(dates_samples_energies_jjs_angles))
+
+        for date_sample_energy_jj_angle in dates_samples_energies_jjs_angles:
+            date = date_sample_energy_jj_angle[0]
+            sample = date_sample_energy_jj_angle[1]
+            energy = date_sample_energy_jj_angle[2]
+            jj_u = date_sample_energy_jj_angle[3]
+            jj_d = date_sample_energy_jj_angle[4]
+            angle = date_sample_energy_jj_angle[5]
+
+            # Raw image records by given date, sample and energy
+            query_cmd = ((files_query.date == date) &
+                         (files_query.sample == sample) &
+                         (files_query.energy == energy) &
+                         (files_query.jj_u == jj_u) &
+                         (files_query.jj_d == jj_d) &
+                         (files_query.angle == angle))
+            img_records = file_index_db.search(query_cmd)
+            num_repetitions = len(img_records)
+            files = get_file_paths(img_records, root_path)
+            complete_group_to_average = [num_repetitions]
+            group_to_average = []
+            for file in files:
+                group_to_average.append(file)
+            complete_group_to_average.append(group_to_average)
+            complete_group_to_average.append(
+                date_sample_energy_jj_angle)
+            groups_to_average.append(complete_group_to_average)
 
     if groups_to_average[0][1]:
         records = Parallel(n_jobs=cores, backend="multiprocessing")(
@@ -208,6 +283,12 @@ def average_image_groups(file_index_fn, table_name="hdf5_proc",
     print("--- Average %d files by groups, took %s seconds ---\n" %
           (n_files, (time.time() - start_time)))
 
+    # import pprint
+    # pobj = pprint.PrettyPrinter(indent=4)
+    # print("----")
+    # print("average records")
+    # for record in records:
+    #     pobj.pprint(record)
     db.close()
 
 
