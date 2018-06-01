cffb8f4825f10489b6c77620aaea22b024d0c561
mrosanes <mrosanes@cells.es>
2018-06-01 14:36:55 +0200

    Allow parsing jj positions
    Allow parsing jj positions. This is useful in
    magnetism experiments, for instance; where the light polarization
    has to be choosen, and this can be done by setting the JJ offset
    to different positions. With changes in the jj positions,
    we can find the circular left and right polarizations.
    
    This new parsing, will allow to preprocess data collected for
    magnetism, many repetitions per angle, experiments.

--- txm2nexuslib/parser.py
+++ txm2nexuslib/parser.py
@@ -48,6 +48,10 @@ class ParserTXMScript(object):
         self.angle = -1000
         self.previous_zpz = 88888888
         self.zpz = 99999999
+        self.previous_jj_u = -8888888
+        self.jj_u = -9999999
+        self.previous_jj_d = -8888888
+        self.jj_d = -9999999
         self.previous_FF = False
         self.FF = False
         self.repetition = 0
@@ -67,6 +71,16 @@ class ParserTXMScript(object):
         word_list = line.split()
         self.zpz = round(float(word_list[-1]), 1)
         self.parameters['zpz'] = self.zpz
+
+    def parse_jj_u(self, line):
+        word_list = line.split()
+        self.jj_u = round(float(word_list[-1]), 1)
+        self.parameters['jj_u'] = self.jj_u
+
+    def parse_jj_d(self, line):
+        word_list = line.split()
+        self.jj_d = round(float(word_list[-1]), 1)
+        self.parameters['jj_d'] = self.jj_d
 
     def parse_subfolder(self, line):
         """Subfolder where the raw data file should be located"""
@@ -117,22 +131,27 @@ class ParserTXMScript(object):
             self.energy == self.previous_energy and
             self.angle == self.previous_angle and
             self.zpz == self.previous_zpz and
+            self.jj_d == self.previous_jj_d and
+            self.jj_u == self.previous_jj_u and
+            self.zpz == self.previous_zpz and
             self.FF == self.previous_FF):
             self.repetition += 1
         else:
             self.repetition = 0
         self.parameters['repetition'] = self.repetition
-
         self.previous_date = self.date
         self.previous_sample = self.sample
         self.previous_energy = self.energy
         self.previous_angle = self.angle
         self.previous_zpz = self.zpz
+        self.previous_jj_d = self.jj_d
+        self.previous_jj_u = self.jj_u
         self.previous_FF = self.FF
         self.parameters['processed'] = False
 
         store_parameters = copy.deepcopy(self.parameters)
         self.collected_files.append(store_parameters)
+        print(self.parameters)
 
     def parse_script(self, txm_txt_script):
         f = open(txm_txt_script, 'r')
@@ -146,10 +165,13 @@ class ParserTXMScript(object):
                 self.parse_zpz(line)
             if "moveto folder" in line:
                 self.parse_subfolder(line)
+            if "moveto phx" in line:
+                self.parse_jj_u(line)
+            if "moveto phy" in line:
+                self.parse_jj_d(line)
             if "collect" in line:
                 self.parse_collect(line)    
         return self.collected_files
-                
 
 def get_db(txm_txt_script, use_existing_db=False):
     """Get the data files DataBase if exisiting, or create the DataBase
