bfc4a54da1bc1b14b5025e5cf6e2f2e652477f6c
mrosanes <mrosanes@cells.es>
2019-08-27 18:23:24 +0200

    Add functions to apply minus log to stacks
    Add functions to convert hdf5 stacks to mrc stacks and to
    compute the absorbance stacks (minus log of the stacks).
    The input and output for applying the minus log, is in mrc.

--- txm2nexuslib/stack/stack_operate.py
+++ txm2nexuslib/stack/stack_operate.py
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import h5py
+import numpy as np
 from tinydb import TinyDB
 
 try:
@@ -45,8 +46,8 @@ def hdf5_2_mrc_stack(h5_stack_fn,
     n_cols = infoshape[2]
 
     # Create empty mrc file
-    outfile_fn = h5_stack_fn.rsplit('.', 1)[0] + '.mrc'
-    mrc_outfile = mrcfile.new_mmap(outfile_fn,
+    mrc_stack_fn = h5_stack_fn.rsplit('.', 1)[0] + '.mrc'
+    mrc_outfile = mrcfile.new_mmap(mrc_stack_fn,
                                    shape=(n_frames, n_rows, n_cols),
                                    mrc_mode=2,
                                    overwrite=True)
@@ -56,6 +57,8 @@ def hdf5_2_mrc_stack(h5_stack_fn,
         mrc_outfile.data[n_img, :, :] = h5_group[dataset][n_img]
     mrc_outfile.flush()
     mrc_outfile.close()
+    print("Stack {} has been converted to mrc".format(mrc_stack_fn))
+    return mrc_stack_fn
 
 
 def hdf5_2_mrc_stacks(db_filename, table_name="hdf5_stacks"):
@@ -64,6 +67,78 @@ def hdf5_2_mrc_stacks(db_filename, table_name="hdf5_stacks"):
     print("Converting multiple hdf5 stacks to mrc")
     db = TinyDB(db_filename)
     stack_table = db.table(table_name)
+    mrc_stack_table = db.table("mrc_stacks")
+    mrc_stack_table.purge()
     for record in stack_table.all():
         h5_stack_fn = record["filename"]
-        hdf5_2_mrc_stack(h5_stack_fn)
+        print(h5_stack_fn)
+        mrc_stack_fn = hdf5_2_mrc_stack(h5_stack_fn)
+        record_mrc = record.copy()
+        record_mrc["filename"] = mrc_stack_fn
+        record_mrc["extension"] = ".mrc"
+        mrc_stack_table.insert(record_mrc)
+    print("")
+
+    """
+    import pprint
+    pretty_printer = pprint.PrettyPrinter(indent=4)
+    print("Created stacks:")
+    for record in stack_table.all():
+        pretty_printer.pprint(record["filename"])
+        pretty_printer.pprint(record["extension"])
+    for record in mrc_stack_table.all():
+        pretty_printer.pprint(record["filename"])
+        pretty_printer.pprint(record["extension"])
+    """
+
+    db.close()
+
+
+def deconvolve_stack(stack_fn):
+    """Deconvolve an mrc stack"""
+    pass
+
+
+def minus_ln_stack_mrc(mrc_stack_fn):
+    """Compute absorbance stack: Apply minus logarithm to a mrc stack"""
+
+    mrc_handler = mrcfile.open(mrc_stack_fn, mode='r')
+
+    infoshape = mrc_handler.data.shape
+    n_frames = infoshape[0]
+    n_rows = infoshape[1]
+    n_cols = infoshape[2]
+
+    # Create empty mrc file
+    mrc_ln_stack_fn = mrc_stack_fn.rsplit('.', 1)[0] + '_ln.mrc'
+    mrc_outfile = mrcfile.new_mmap(mrc_ln_stack_fn,
+                                   shape=(n_frames, n_rows, n_cols),
+                                   mrc_mode=2,
+                                   overwrite=True)
+
+    # Calculate absorbance (minus logarithm) stack and store on mrc
+    for n_img in range(n_frames):
+        img = mrc_handler.data[n_img, :, :]
+        mrc_outfile.data[n_img, :, :] = -np.log(img)
+    mrc_outfile.flush()
+    mrc_outfile.close()
+
+    print("Minus logarithm applied on stack {}".format(mrc_stack_fn))
+    return mrc_ln_stack_fn
+
+
+def minus_ln_stacks_mrc(db_filename, table_name="mrc_stacks"):
+    """Compute absorbance stacks (by applying the minus natural logarithm)
+    to multiple mrc stacks.
+    """
+
+    print("Compute absorbance stacks by applying the minus natural logarithm:")
+    db = TinyDB(db_filename)
+    mrc_stack_table = db.table(table_name)
+    for record_mrc in mrc_stack_table.all():
+        mrc_ln_stack_fn = minus_ln_stack_mrc(record_mrc["filename"])
+        record_ln_mrc = record_mrc.copy()
+        record_ln_mrc["filename"] = mrc_ln_stack_fn
+        record_ln_mrc["extension"] = ".mrc"
+        mrc_stack_table.insert(record_ln_mrc)
+    print("")
