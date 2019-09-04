995c3370b7fa771d90f2ca021764db48f18f3ef8
mrosanes <mrosanes@cells.es>
2019-09-04 17:40:09 +0200

    Bump version 12.0.0-alpha to 12.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '12.0.0-alpha'
+__version = '12.0.0'
 
 long_description = """bl09-imaging: Image/Signal processing for BL09-Mistral 
 Synchrotron Beamline Laboratory (formats: hdf5, mrc, xrm/txrm). 
