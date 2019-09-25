7e54cc9c7941d145b7bd3a30b356e2c95602d6c7
mrosanes <mrosanes@cells.es>
2019-09-25 17:18:49 +0200

    Add option to normalize using a DarkField
    Add option to normalize using a DarkField in addition to the
    FF normalization.
    
    Formula:
    norm = [img/(eti*mci) - DF/etdf]  /  [FF/(etff*mcff) - DF/etdf]
    
    Where:
        norm: normalized single sample image in stack
        img: single sample image in stack
        eti: exposure time sample image
        mci: machine current sample image
        FF: averaged FF image (average of all FF images of FF stack)
        etff: exposure time flat-field
        mcff: machine current flat-field
        DF: averaged DF image (average of all DF images of FF stack)
        etdf: exposure time dark-field

--- txm2nexuslib/scripts/normalize.py
+++ txm2nexuslib/scripts/normalize.py
@@ -20,10 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 
+import argparse
+from argparse import RawTextHelpFormatter
+
 from txm2nexuslib import tomonorm
 from txm2nexuslib import specnorm
 from txm2nexuslib import mosaicnorm
-import argparse
 
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
@@ -32,53 +34,59 @@ class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
 
 
 def main():
-    parser = argparse.ArgumentParser(description=
-                                     "Normalization of: \n" +
-                                     "- TOMOGRAPHIES \n" +
-                                     "- SPECTROSCOPIES\n "
-                                     "Taking into account FF, currents and "
-                                     "exposure times",
-                                     formatter_class=CustomFormatter)
+    parser = argparse.ArgumentParser(
+        description="Normalization of:"
+                    + "\n- TOMOGRAPHIES for single energy & single focus"
+                    + "\n- SPECTROSCOPIES"
+                    + "\nTaking into account FF, currents and exposure times",
+        formatter_class=RawTextHelpFormatter)
 
     parser.add_argument('inputfile', type=str, default=None,
-                        help='Enter hdf5 file which contains the information ' +
-                             'of both tomography and flatfield.')
+                        help='Enter hdf5 file containing the'
+                             + '\ninformation of tomography data, flatfield,'
+                             + '\nand optionally darkfield.')
 
     parser.add_argument('-s', '--spectroscopy', type=int, default=0,
-                        help='Constant energy tomo normalization (0) ' +
-                             'or spectroscopy normalization (-s=1).')
+                        help='Constant energy tomo normalization (-s=0)'
+                             + '\nor spectroscopy normalization (-s=1).'
+                             + '\nDefault: -s=0.')
+
+    parser.add_argument('-d', '--darkfield', type=int, default=0,
+                        help='Normalize using also a DarkField'
+                             + '\nDefault: -d=0.')
 
     parser.add_argument('-g', '--gaussianblur', type=int, default=0,
                         help='gaussian filtering for avoiding '
-                             'diffraction artifacts. \n' +
-                             'Default=0 -> gaussian filter not applied. \n' +
-                             'Integer not 0: Indicate the std as integer. '
-                             'Ex: -g=5')
+                             + 'diffraction artifacts.'
+                             + '\nDefault=0 -> gaussian filter not applied.'
+                             + '\nInteger not 0: Indicate the std as integer. '
+                             + '\nE.g.: -g=5')
 
     parser.add_argument('-f', '--avgff', type=int, default=1,
-                        help='Normalize using avergeFF (-f=1); or using ' +
-                             'single FF (FFimage 0) (-f=0).')
+                        help='Normalize using avergeFF (-f=1)'
+                             + '\nor using single FF (FFimage 0)'
+                             + '\n(Default: -f=1).')
 
     parser.add_argument('-m', '--mosaicnorm', type=int, default=0,
                         help='Mosaic normalization using a given FF (-m=1).')
     
     parser.add_argument('-r', '--ratio', type=int, default=1,
-                        help='ratio = exp_time_mosaic/exp_time_FF.\n' +
-                             'Exposure times ratio. \n' +
-                             'This option can be used only when '
-                             'normalizing mosaics.')
+                        help='ratio = exp_time_mosaic/exp_time_FF.'
+                             + '\nExposure times ratio. '
+                             + '\nThis option can be used only when '
+                             + 'normalizing mosaics.')
                
     parser.add_argument('-a', '--avgtomnorm', type=int, default=0,
-                        help='Indicate if we want to obtain the average of '
-                             'the normalized images (-a=1).\n '
-                             'Available only for Tomo normalization.')
+                        help='Indicate if we want to obtain the average of'
+                             + '\nthe normalized images (-a=1).'
+                             + '\nAvailable only for Tomo normalization.')
 
-    parser.add_argument('-d',
+    parser.add_argument('-di',
                         '--diffraction',
                         type=int,
                         default=0,
-                        help='Correct diffraction pattern with external '
-                             'given avgFF (-d=1).')
+                        help='Correct diffraction pattern by passing'
+                             + '\nan external avgFF (-d=1).')
 
     args = parser.parse_args()
 
@@ -94,11 +102,9 @@ def main():
             """ We normalize the tomography using the tomography images,
             the FF (flatfield) images, the experimental times of FF, images, 
             and the machine current for each image."""
-            normalize_object = tomonorm.TomoNormalize(args.inputfile,
-                                                      args.avgtomnorm,
-                                                      args.gaussianblur,
-                                                      args.avgff,
-                        args.diffraction)
+            normalize_object = tomonorm.TomoNormalize(
+                args.inputfile, args.darkfield, args.avgtomnorm,
+                args.gaussianblur, args.avgff, args.diffraction)
             normalize_object.normalize_tomo()
         else:
             print("\nNormalizing Spectroscopy images")
--- txm2nexuslib/tomonorm.py
+++ txm2nexuslib/tomonorm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-(C) Copyright 2014-2017 Marc Rosanes
+(C) Copyright 2014 Marc Rosanes Siscart
 The program is distributed under the terms of the 
 GNU General Public License (or the Lesser GPL).
 
@@ -25,15 +25,17 @@ import h5py
 
 class TomoNormalize:
 
-    def __init__(self, inputfile, avgtomnorm, gaussianblur, avgff, diffraction):
+    def __init__(self, inputfile, darkfield, avgtomnorm,
+                 gaussianblur, avgff, diffraction):
 
-        self.avgtomnorm = avgtomnorm
         self.filename_nexus = inputfile
         self.input_nexusfile = h5py.File(self.filename_nexus, 'r')
         self.outputfilehdf5 = inputfile.rsplit('.', 1)[0] + '_norm.hdf5'
         self.tomonorm = h5py.File(self.outputfilehdf5, 'w')
         self.norm_grp = self.tomonorm.create_group("TomoNormalized")
         self.norm_grp.attrs['NX_class'] = "NXentry"
+        self.darkfield = darkfield
+        self.avgtomnorm = avgtomnorm
 
         # Angles and Energies
         self.energies = list()
@@ -46,9 +48,9 @@ class TomoNormalize:
 
         # Ratios for the currents.
         self.ratios_currents_tomo = list()
-        self.ratios_currents_flatfield = list()  
-           
-        # Note: flatfield= FlatField= FF   
+        self.ratios_currents_flatfield = list()
+
+        # FF data & metadata (Note: FF = FlatField  = BrightField)
         self.exptimes_FF = 0
         self.avg_ff_exptime = 0
         self.currents_flatfield = 0
@@ -57,14 +59,27 @@ class TomoNormalize:
         self.numrowsFF = 0
         self.numcolsFF = 0
 
+        # DF data & metadata (Note: DF = DarkField)
+        self.exptimes_DF = 0
+        self.avg_df_exptime = 0
+        self.currents_darkfield = 0
+        self.data_darkfield = 0
+        self.nFramesDF = 0
+        self.numrowsDF = 0
+        self.numcolsDF = 0
+
+        # Sample data & metadata
         self.exposuretimes_tomo = 0
         self.currents_tomo = 0
         self.data_tomo = 0
         self.nFramesSample = 0                
         self.numrows = 0
         self.numcols = 0
-        
+
+        # FF: FlatField (a.k.a. BrightField)
         self.averageff = 0
+        # DF: DarkField
+        self.averagedf = 0
 
         self.boolean_current_exists = 0
         self.avgff = avgff
@@ -76,6 +91,8 @@ class TomoNormalize:
 
         nxtomo_grp = self.input_nexusfile["NXtomo"]
         instrument_grp = nxtomo_grp["instrument"]
+        if "dark_field" in instrument_grp:
+            self.darkfield = 1
 
         #####################
         # Retrieving Angles #
@@ -111,14 +128,25 @@ class TomoNormalize:
             self.boolean_current_exists = 0
             print("\nCurrents could not be extracted.\n")
 
+        ##########################
+        # Retrieving FF Currents #
+        ##########################
+        FF_grp = instrument_grp["bright_field"]
+        if self.boolean_current_exists:
+            self.currents_flatfield = FF_grp["current"]
+            self.norm_grp.create_dataset("CurrentsFF",
+                                         data=self.currents_flatfield)
+
         #########################
         # Retrieving Pixel Size #
         #########################
         try:
             self.x_pixel_size = instrument_grp["sample"]["x_pixel_size"].value
             self.y_pixel_size = instrument_grp["sample"]["y_pixel_size"].value
-            self.norm_grp.create_dataset("x_pixel_size", data=self.x_pixel_size)
-            self.norm_grp.create_dataset("y_pixel_size", data=self.y_pixel_size)
+            self.norm_grp.create_dataset(
+                "x_pixel_size", data=self.x_pixel_size)
+            self.norm_grp.create_dataset(
+                "y_pixel_size", data=self.y_pixel_size)
         except:
             print("\nPixel size could NOT be extracted.\n")
 
@@ -139,10 +167,10 @@ class TomoNormalize:
         self.numrows = infoshape[1]
         self.numcols = infoshape[2]
 
-        # FF Data
-        FF_grp = instrument_grp["bright_field"]
+        # FF Data (FlatField/BrightField data)
         self.data_flatfield = FF_grp["data"].value
-        self.exptimes_FF = FF_grp["ExpTimes"]
+        self.exptimes_FF = FF_grp["ExpTimes"].value
+        self.norm_grp['ExpTimesFF'] = self.exptimes_FF
         dimensions_singleimage_flatfield = self.data_flatfield[0].shape
 
         self.ratios_exptimes = [None] * num_exptimes_tomo
@@ -158,36 +186,54 @@ class TomoNormalize:
         self.numrowsFF = self.data_flatfield.shape[1]
         self.numcolsFF = self.data_flatfield.shape[2]
 
+        # DF Data (DarkField data)
+        if self.darkfield:
+            DF_grp = instrument_grp["dark_field"]
+            self.data_darkfield = DF_grp["data"].value
+            self.exptimes_DF = DF_grp["ExpTimes"].value
+            self.norm_grp['ExpTimesDF'] = self.exptimes_DF
+            dimensions_singleimage_darkfield = self.data_darkfield[0].shape
+
+            if dimensions_singleimage_tomo != \
+                    dimensions_singleimage_darkfield:
+                msg = ("\nThe dimensions of a tomography image does not"
+                       + " correspond with the FF image dimensions."
+                       + "\nThe normalization cannot be done.")
+                raise msg
+
+            # Average of the DF exposure times.
+            for i in range(len(self.exptimes_DF)):
+                self.avg_df_exptime += self.exptimes_DF[i]
+            self.avg_df_exptime /= len(self.exptimes_DF)
+            print('DarkField Exposure Time is {0}\n'.format(
+                self.avg_df_exptime))
+
+            self.nFramesDF = self.data_darkfield.shape[0]
+            self.numrowsDF = self.data_darkfield.shape[1]
+            self.numcolsDF = self.data_darkfield.shape[2]
+
         if dimensions_singleimage_tomo == \
                 dimensions_singleimage_flatfield:
 
             self.norm_grp.create_dataset(
                 "TomoNormalized",
-                shape=(self.nFramesSample,
-                       self.numrows,
-                       self.numcols),
-                chunks=(1,
-                        self.numrows,
-                        self.numcols),
+                shape=(self.nFramesSample, self.numrows, self.numcols),
+                chunks=(1, self.numrows, self.numcols),
                 dtype='float32')
 
             self.norm_grp['TomoNormalized'].attrs['Number of Frames'] = \
                 self.nFramesSample
 
-            avgnormalizedtomo = np.zeros((self.numrows,
-                                          self.numcols),
-                                         dtype=np.float)
+            avgnormalizedtomo = np.zeros(
+                (self.numrows, self.numcols), dtype=np.float)
 
             self.averageff = np.zeros((self.numrowsFF, self.numcolsFF),
                                       dtype=np.float)
 
-            if self.boolean_current_exists == 1:
+            if self.boolean_current_exists and not self.darkfield:
                 print('\nInformation about currents is present in hdf5 file')
                 print('Tomography will be normalized taking into account '
                       'the ExposureTimes and the MachineCurrents\n')
-
-                # Get FF Currents
-                self.currents_flatfield = FF_grp["current"]
 
                 num_currents_tomo = len(self.currents_tomo)
                 self.ratios_currents_tomo = [None]*num_currents_tomo
@@ -197,8 +243,6 @@ class TomoNormalize:
 
                 if self.avgff == 1:
                     self.norm_grp['Avg_FF_ExpTime'] = self.avg_ff_exptime
-                self.norm_grp.create_dataset("CurrentsFF",
-                                             data=self.currents_flatfield)
 
                 # Getting the Ratios
                 for i in range(num_exptimes_tomo):
@@ -216,24 +260,20 @@ class TomoNormalize:
                 # FlatField (FF) images normalized with current,
                 # and Average of FlatField Normalized with current
                 self.norm_grp.create_dataset(
-                    "FFNormalizedWithCurrent",
-                    shape=(self.nFramesFF,
-                           self.numrowsFF,
-                           self.numcolsFF),
-                    chunks=(1,
-                            self.numrowsFF,
-                            self.numcolsFF),
+                    "FFNormalized",
+                    shape=(self.nFramesFF, self.numrowsFF, self.numcolsFF),
+                    chunks=(1, self.numrowsFF, self.numcolsFF),
                     dtype='float32')
 
-                dset_FF_norm_current = self.norm_grp["FFNormalizedWithCurrent"]
-                dset_FF_norm_current.attrs['Number of Frames'] = self.nFramesFF
+                self.norm_grp["FFNormalized"].attrs[
+                    'Number of Frames'] = self.nFramesFF
 
                 for numimgFF in range(self.nFramesFF):
                     image_FF_normalized_with_current = np.array(
                         self.data_flatfield[numimgFF] /
                         self.ratios_currents_flatfield[numimgFF],
                         dtype=np.float)
-                    self.norm_grp['FFNormalizedWithCurrent'][numimgFF] = \
+                    self.norm_grp['FFNormalized'][numimgFF] = \
                         image_FF_normalized_with_current
 
                     if self.avgff == 1:
@@ -243,9 +283,10 @@ class TomoNormalize:
                           'machine_currents' % numimgFF)
 
                 if self.avgff == 0:
-                    self.averageff = np.array(self.data_flatfield[0] /
-                                              self.ratios_currents_flatfield[0],
-                                              dtype=np.float)
+                    self.averageff = np.array(
+                        self.data_flatfield[0] /
+                        self.ratios_currents_flatfield[0],
+                        dtype=np.float)
                     print('\nFFs have been calculated '
                           'using the machine_currents\n')
 
@@ -259,7 +300,7 @@ class TomoNormalize:
 
                 if self.avgff == 1:
                     self.averageff = self.averageff/self.nFramesFF
-                    if(self.gaussianblur != 0):
+                    if self.gaussianblur != 0:
                         from scipy import ndimage
                         self.averageff = ndimage.gaussian_filter(
                                        self.averageff, sigma=self.gaussianblur)
@@ -270,7 +311,8 @@ class TomoNormalize:
                 for numimg in range(self.nFramesSample):
                     individual_image = sample_image_data[numimg]
                     normalizedtomo_singleimage = np.array(
-                        ((individual_image / self.ratios_currents_tomo[numimg]) /
+                        ((individual_image /
+                          self.ratios_currents_tomo[numimg]) /
                          (self.averageff*self.ratios_exptimes[numimg])),
                         dtype=np.float32)
                     self.norm_grp['TomoNormalized'][numimg] = \
@@ -280,8 +322,87 @@ class TomoNormalize:
                     if numimg%10 == 0:
                         print('Image %d has been normalized' % numimg)
 
-            else:
+            elif self.boolean_current_exists and self.darkfield:
+                print('\nInformation about currents is present in hdf5 file')
+                print('\nTomography will be normalized taking into account'
+                      + '\n the ExposureTimes, the MachineCurrents,'
+                      + '\n the FlatFields and the DarkFields\n')
 
+                self.norm_grp['Avg_FF_ExpTime'] = self.avg_ff_exptime
+                self.norm_grp['Avg_DF_ExpTime'] = self.avg_df_exptime
+
+                # FlatField (FF) images normalized with its relative
+                # currents and exposure times:
+                self.norm_grp.create_dataset(
+                    "FFNormalized",
+                    shape=(self.nFramesFF, self.numrowsFF, self.numcolsFF),
+                    chunks=(1, self.numrowsFF, self.numcolsFF),
+                    dtype='float32')
+
+                self.norm_grp["FFNormalized"].attrs[
+                    'Number of Frames'] = self.nFramesFF
+
+                for numimgFF in range(self.nFramesFF):
+                    denominator = (self.currents_flatfield[numimgFF]
+                                   * self.exptimes_FF[numimgFF])
+                    image_FF_normalized = np.array(
+                        self.data_flatfield[numimgFF] / denominator,
+                        dtype=np.float)
+                    self.norm_grp['FFNormalized'][
+                        numimgFF] = image_FF_normalized
+                    self.averageff += image_FF_normalized
+
+                self.averageff = self.averageff/self.nFramesFF
+                if self.gaussianblur != 0:
+                    from scipy import ndimage
+                    self.averageff = ndimage.gaussian_filter(
+                        self.averageff, sigma=self.gaussianblur)
+                self.norm_grp['NormalizedFF'] = self.averageff
+                print('\nFF has been normalized using FF machine_currents'
+                      + ' and FF exposure times\n')
+
+                # DarkField (DF) images normalized with its relative
+                # exposure times:
+                self.norm_grp.create_dataset(
+                    "DFNormalized",
+                    shape=(self.nFramesDF, self.numrowsDF, self.numcolsDF),
+                    chunks=(1, self.numrowsDF, self.numcolsDF),
+                    dtype='float32')
+
+                self.norm_grp["DFNormalized"].attrs[
+                    'Number of Frames'] = self.nFramesDF
+
+                for numimgDF in range(self.nFramesDF):
+                    image_DF_normalized = np.array(
+                        self.data_darkfield[numimgDF]
+                        / self.exptimes_DF[numimgDF], dtype=np.float)
+                    self.norm_grp['DFNormalized'][
+                        numimgDF] = image_DF_normalized
+                    self.averagedf += image_DF_normalized
+
+                self.averagedf = self.averagedf/self.nFramesDF
+                self.norm_grp['NormalizedDF'] = self.averagedf
+                print('\nNormalizedDF has been normalized'
+                      ' using DF exposure times\n')
+
+                # Normalize images by applying:
+                # norm[i] =  [  ( img[i]/(eti*mci) - DF/etdf ) /
+                #               ( FF/(etff*mcff) - DF/etdf )     ]
+                for numimg in range(self.nFramesSample):
+                    individual_image = sample_image_data[numimg]
+                    den_img = (self.exposuretimes_tomo[numimg]
+                           * self.currents_tomo[numimg])
+                    numerator = individual_image / den_img - self.averagedf
+                    denominator = self.averageff - self.averagedf
+                    normalizedtomo_singleimage = np.array(
+                        (numerator / denominator), dtype=np.float32)
+                    self.norm_grp['TomoNormalized'][numimg] = \
+                        normalizedtomo_singleimage
+                    avgnormalizedtomo += normalizedtomo_singleimage
+                    if numimg%10 == 0:
+                        print('Image %d has been normalized' % numimg)
+
+            elif not self.boolean_current_exists:
                 print('\nInformation about currents is NOT present '
                       'in hdf5 file')
                 print('Tomography will be normalized taking into account '
